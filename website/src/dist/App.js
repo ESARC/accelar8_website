90b228183224f0c79af14d68fd89fcbe2ae58a92
Vaibhav Malhotra <vbvrocks84@gmail.com>
2023-05-04 00:38:14 -0700

    clean up

--- website/src/dist/App.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-require("./App.css");
-function App() {
-    return (React.createElement("div", { className: "App" },
-        React.createElement("header", { className: "App-header" })));
-}
-exports["default"] = App;
