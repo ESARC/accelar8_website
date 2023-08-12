0611e92cc1208ddbeee69648f1d60359bfec7d9b
Bhimesh Chauhan <bhimeshchauhan@gmail.com>
2023-08-12 12:52:55 -0400

    mend

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,27 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
+
+export const metadata = {
+  title: 'DigiAuto',
+  description:
+    'DigiAuto is here to make your car buying experience as easy as possible.',
+};
+
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="robots" content="index, follow" />
+        <meta name="description" content={metadata.description} />
+        <meta property="og:title" content={metadata.title} />
+        <meta property="og:description" content={metadata.description} />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content={metadata.title} />
+        <meta property="og:url" content="https://digiauto.dev/" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
