adc6743591dc8443f499f85c016f5f5eb3e8170d
Bhimesh Chauhan <bhimeshchauhan@gmail.com>
2023-08-16 21:21:24 -0400

    move-utils-from-pages

--- src/pages/utils.tsx
+++
@@ -1,7 +0,0 @@
-const metadata = {
-  title: "DigiAuto",
-  description:
-    "DigiAuto is here to make your car buying experience as easy as possible.",
-};
-
-export default metadata;
---
+++ src/pages/utils/index.ts
@@ -0,0 +1,7 @@
+const metadata = {
+    title: "DigiAuto",
+    description:
+      "DigiAuto is here to make your car buying experience as easy as possible.",
+  };
+  
+  export default metadata;
